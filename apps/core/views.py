3a1b1384354bf86e90a85e021dbc2aaf5a83855b
Augusto Cesar Albuquerque Tavares <88669775+acatcesar@users.noreply.github.com>
2023-04-25 16:28:08 -0300

    final 24.04

--- apps/core/views.py
+++ apps/core/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from apps.funcionarios.models import Funcionario
+from apps.registro_hora_extra.models import RegistroHoraExtra
+from django.db.models import Sum
 
 
 @login_required
@@ -11,4 +13,9 @@ def home(request):
     data['total_funcionarios'] = funcionario.empresa.total_funcionarios
     data['total_funcionarios_ferias'] = funcionario.empresa.total_funcionarios_ferias
     data['total_funcionarios_doc_pendente'] = funcionario.empresa.total_funcionarios_doc_pendente
+    data['total_funcionarios_rg'] = 10
+    data['total_hora_extra_utilizadas'] = RegistroHoraExtra.objects.filter(
+        funcionario__empresa=funcionario.empresa, utilizada=True).aggregate(Sum('horas'))['horas__sum'] or 0
+    data['total_hora_extra_pendente'] = RegistroHoraExtra.objects.filter(
+        funcionario__empresa=funcionario.empresa, utilizada=False).aggregate(Sum('horas'))['horas__sum'] or 0
     return render(request, 'core/index.html', data)
