2dc4959cfda4e6dfc7708750fb713ab7e41fd612
Augusto Cesar Albuquerque Tavares <88669775+acatcesar@users.noreply.github.com>
2023-09-21 15:39:32 -0300

    Ajuste nas imagens, e api 21/01/2023

--- apps/core/urls.py
+++ apps/core/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path, include
 from .views import home
+from . import views
 
 urlpatterns = [
     path('', home, name='home'),
+    path('user/<str:username>/', views.get_github_user_info, name='github_user_info'),
+    path('user/', views.get_cotacao, name='get_cotacao'),
+
 ]
--- apps/core/views.py
+++ apps/core/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth.decorators import login_required
 from apps.funcionarios.models import Funcionario
 from apps.registro_hora_extra.models import RegistroHoraExtra
 from django.db.models import Sum
+import requests
 
 
 @login_required
@@ -19,3 +20,45 @@ def home(request):
     data['total_hora_extra_pendente'] = RegistroHoraExtra.objects.filter(
         funcionario__empresa=funcionario.empresa, utilizada=False).aggregate(Sum('horas'))['horas__sum'] or 0
     return render(request, 'core/index.html', data)
+
+
+
+
+import requests
+from django.http import JsonResponse
+
+def get_github_user_info(request, username):
+    username = 'acatcesar'
+    url = f'https://api.github.com/users/{username}'
+    response = requests.get(url)
+
+
+    if response.status_code == 200:
+        data = response.json()
+        return JsonResponse({'Nome completo': data['name']})
+    else:
+        return JsonResponse({'mensagem': 'Usuário não encontrado'}, status=404)
+
+
+# def get_cotacao(request):
+#     token = 'hq5CAHPZYezxFJYXRwPyHE'
+#     url = f'https://brapi.dev/api/quote/PETR4%2C%5EBVSP?range=1d&interval=1d&fundamental=true&dividends=true'
+#     response = requests.get(url)
+#
+#     if response.status_code == 200:
+#         data = response.json()
+#         print(data)
+#         return JsonResponse( data)
+#     else:
+#         return JsonResponse({'mensagem': 'Usuário não encontrado'}, status=404)
+
+def get_cotacao(request):
+    token = 'hq5CAHPZYezxFJYXRwPyHE'
+    url = f'https://brapi.dev/api/quote/PETR4?token={token}'
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        data = response.json()
+        return JsonResponse(data)
+    else:
+        return JsonResponse({'mensagem': 'Ação não encontrata'}, status=404)
